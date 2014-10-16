986e62ff14dc942366085aec5237dfe669682b1b
Kaminuri <matt-picard@hotmail.Fr>
2014-10-17 01:34:22 +0200

    Add methode Cellule/base

--- VirtualWar/src/game/Base.java
+++ VirtualWar/src/game/Base.java
@@ -1,5 +1,30 @@
 package game;
 
+import entity.Robot;
+
 public class Base extends Cellule{
+
+	public Base(int l, int h) {
+		super(l, h);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	void deplaceSur(Robot robot) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void ajoute(int equipe) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void videCase() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- VirtualWar/src/game/Cellule.java
+++ VirtualWar/src/game/Cellule.java
@@ -1,5 +1,32 @@
 package game;
 
-public class Cellule {
+import entity.Robot;
+import move.Coordonnees;
 
+public abstract class Cellule {
+	protected int mine =0;
+	protected int base=0;
+	private Coordonnees c;
+	private Robot robot;
+	String image;
+	public Cellule(int l, int h){
+	}
+	public int contientMine(){
+		return mine;
+	}
+	public int estBase(){
+		return base;
+	}
+	public Coordonnees getCoordonnees(){
+		return c;
+	}
+	public Robot getContenu(){
+		return robot;
+	}
+	public String toString(){
+		return "stfu";
+	}
+	abstract void deplaceSur(Robot robot);
+	abstract void ajoute(int equipe);
+	abstract void videCase();
 }
