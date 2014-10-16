84e801a3dd85f987aa6ca552fc4f8c5ea582d771
Kaminuri <matt-picard@hotmail.Fr>
2014-10-17 01:26:54 +0200

    Add methode entity

--- VirtualWar/src/entity/Robot.java
+++ VirtualWar/src/entity/Robot.java
@@ -1,5 +1,43 @@
 package entity;
 
-public abstract class Robot {
+import java.util.List;
 
+import move.Coordonnees;
+import game.Vue;
+
+public abstract class Robot {
+	private int energie;
+	private int equipe;
+	private Coordonnees c;
+	public Robot(Vue vue, int l, int h, int equipe){
+	}
+	abstract public boolean peutTirer();
+	public abstract int getCoutAction();
+	public abstract int getCoutDep();
+	public abstract int getDegatTir();
+	public abstract int getDegatMine();
+	public abstract String getType();
+	public abstract public List<Coordonnees> getDeplacement();
+	
+	public boolean estSurBase(){return false;}
+	
+	public int getEnergie() {
+		return energie;
+	}
+	public void setEnergie(int energie) {
+		this.energie = energie;
+	}
+	public int getEquipe() {
+		return equipe;
+	}
+	public void setEquipe(int equipe) {
+		this.equipe = equipe;
+	}
+	public Coordonnees getCoordonnees() {
+		return c;
+	}
+	public void setC(Coordonnees c) {
+		this.c = c;
+	}
+	
 }
--- VirtualWar/src/game/Case.java
+++ VirtualWar/src/game/Case.java
@@ -1,4 +1,4 @@
-package entity;
+package game;
 
 public class Case {
 
