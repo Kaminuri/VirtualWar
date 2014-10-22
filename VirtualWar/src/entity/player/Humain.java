f7f52a25d351b6c60c4028a49d5c5d528eb50483
Kaminuri <matt-picard@hotmail.Fr>
2014-10-22 19:07:24 +0200

    Ajout des classes joueurs

---
+++ VirtualWar/src/entity/player/Humain.java
@@ -0,0 +1,55 @@
+package entity.player;
+
+import entity.Joueur;
+import entity.Robot;
+
+public class Humain extends Joueur{
+
+	public Humain(int equipe) {
+		super(equipe);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getNbTireur() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNbPiegeur() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNbChar() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int choixRobot(int nbRobot) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int choixAction() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int directionAttaque(Robot robot) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int directionDeplacement(Robot robot) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
